e0ed1f36b20216367d3528fb9a164f8c811403d9
paperman1 <powerplant724@gmail.com>
2026-02-03 16:56:56 +0900

    feat: add genai for test

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+gemini_api_key=os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key = gemini_api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="안녕! 한 문장으로 자기소개 해줘"
+)
+
+print(response.text)
